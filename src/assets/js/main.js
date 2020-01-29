56f747865c1f9e8345cd5411df42887cc073b673
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-01-29 16:22:17 +0200

    commit 29.01 v3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -37,110 +37,102 @@ $('.icon-circle_settings').click(function () {
     $('#pills-profile-tab02-01').click();
 });
 
-var myFullpage = new fullpage('#app', {
-    //Navigation
-    menu: '#menu',
-    lockAnchors: false,
-    anchors:[
-        'firstSlide',
-        'ndaSlide',
-        'secondSlide',
-        'thirdSlide',
-        'fourthSlide',
-        'secondSlide',
-        'fiveSlide',
-        'technology',
-        'otherJobs',
-        'writeToUs'
-    ],
-    navigation: false,
-    navigationPosition: 'right',
-    navigationTooltips: null,
-    showActiveTooltip: false,
-    slidesNavigation: false,
-    slidesNavPosition: 'bottom',
+jQuery(document).ready(function($) {
+    $('#app').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:[
+            'url-firstSlide',
+            'url-ndaSlide',
+            'url-secondSlide',
+            'url-thirdSlide',
+            'url-fourthSlide',
+            'url-secondSlide',
+            'url-fiveSlide',
+            'url-technology',
+            'url-otherJobs',
+            'url-writeToUs'
+        ],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
 
-    //Scrolling
-    css3: true,
-    scrollingSpeed: 700,
-    autoScrolling: false,
-    fitToSection: false,
-    fitToSectionDelay: 1000,
-    scrollBar: false,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: true,
-    continuousVertical: false,
-    continuousHorizontal: false,
-    scrollHorizontally: false,
-    interlockedSlides: false,
-    dragAndMove: false,
-    offsetSections: false,
-    resetSliders: false,
-    fadingEffect: false,
-    normalScrollElements: null,
-    scrollOverflow: false,
-    scrollOverflowReset: false,
-    scrollOverflowOptions: {
-        scrollbars: true,
-        mouseWheel: true,
-        hideScrollbars: false,
-        fadeScrollbars: false,
-        disableMouse: true,
-        interactiveScrollbars: true,
-        deceleration: 0.1
-    },
-    touchSensitivity: 15,
-    bigSectionsDestination: null,
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: false,
+        fitToSection: false,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: null,
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: {
+            scrollbars: true,
+            mouseWheel: true,
+            hideScrollbars: false,
+            fadeScrollbars: false,
+            disableMouse: true,
+            interactiveScrollbars: true,
+            deceleration: 0.1
+        },
+        touchSensitivity: 15,
+        bigSectionsDestination: null,
 
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
 
-    //Design
-    controlArrows: true,
-    verticalCentered: true,
-    sectionsColor : 'none',
-    paddingTop: null,
-    paddingBottom: null,
-    fixedElements: null,
-    responsiveWidth: 0,
-    responsiveHeight: 0,
-    responsiveSlides: false,
-    parallax: false,
-    parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
-    cards: false,
-    cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : 'none',
+        paddingTop: null,
+        paddingBottom: null,
+        fixedElements: null,
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+        cards: false,
+        cardsOptions: {perspective: 100, fadeContent: true, fadeBackground: true},
 
-    //Custom selectors
-    sectionSelector: '.section',
-    slideSelector: '.slide',
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
 
-    lazyLoading: true,
+        lazyLoading: true,
 
-    //events
-    onLeave: function(origin, nextIndex, direction){
-        automation(nextIndex, direction)
-    },
-    afterLoad: function(origin, destination, direction){},
-    afterRender: function(){},
-    afterResize: function(width, height){},
-    afterReBuild: function(){},
-    afterResponsive: function(isResponsive){},
-    afterSlideLoad: function(section, origin, destination, direction){},
-    onSlideLeave: function(section, origin, destination, direction){}
+        //events
+        onLeave: function(origin, nextIndex, direction){
+            automation(nextIndex, direction)
+        },
+        afterLoad: function(origin, destination, direction){},
+        afterRender: function(){},
+        afterResize: function(width, height){},
+        afterReBuild: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(section, origin, destination, direction){},
+        onSlideLeave: function(section, origin, destination, direction){}
+    });
 });
-
-
-
-function automation(nextIndex, direction) {
-    if ( nextIndex >= 1 && nextIndex <=8 ) {
-        if ( nextIndex === 1 ) {
-
-        }
-    }
-}
 
