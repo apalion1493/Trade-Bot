ec343d7bc0f7096de0212aed3bd74348ce7bcd6b
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2020-01-31 16:47:17 +0200

    commit 31.01 v2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -40,64 +40,14 @@ $('.icon-circle_settings').click(function () {
 
 let controller = new ScrollMagic.Controller();
 
-let tween3;
-tween3 = TweenMax.to(".third-slide__container-1", 0.5, {opacity : 1, left: '0px', className:'+=stage-first'});
-let scene3 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "300px", offset: 0})
-    .setTween(tween3)
-    .addIndicators()
-    .addTo(controller);
+// let tween3 = TweenMax.to(".third-slide__container-1", 0.5, {className:'+=stage-first'});
 
-let tween3_1;
-tween3_1 = TweenMax.to(".third-slide__container-1", 0.5, {opacity : 0,});
-let scene3_1 = new ScrollMagic.Scene({triggerElement: "#trigger1-1", duration: "200px", offset: 0})
-    .setTween(tween3_1)
-    .addIndicators()
-    .addTo(controller);
-
-/////////////////////
-let tween2;
-tween2 = TweenMax.to(".third-slide__container-2", 0.5, {opacity : 1, left: '0px', className:'+=stage-second'});
-let scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", duration: "300px", offset: 0})
-    .setTween(tween2)
-    .addIndicators()
-    .addTo(controller);
-
-let tween2_1;
-tween2_1 = TweenMax.to(".third-slide__container-2", 0.5, {opacity : 0});
-let scene2_1 = new ScrollMagic.Scene({triggerElement: "#trigger2-1", duration: "200px", offset: 0})
-    .setTween(tween2_1)
-    .addIndicators()
-    .addTo(controller);
+let tween3 = new TimelineMax();
+let tween2 = new TimelineMax();
 
 
-////////////////////////
+tween3.to(".third-slide__container-1", 1.5, {className:'+=stage-first'}).to(".third-slide__container-1", 0, {className:'-=stage-first'}).to(".third-slide__container-1", 1.5, {className:'+=stage-second'}).to(".third-slide__container-1", 0, {className:'-=stage-second'}).to(".third-slide__container-1", 1.5, {className:'+=stage-third'}).to(".third-slide__container-1", 0, {className:'-=stage-third'}).to(".third-slide__container-1", 1.5, {className:'+=stage-fourth'});
+tween2.to(".tab-pane-1", 1.5, {className:'+=show active'}).to(".tab-pane-1", 0, {className:'-=show active'}).to(".tab-pane-2", 1.5, {className:'+=show active'}).to(".tab-pane-2", 0, {className:'-=show active'}).to(".tab-pane-3", 1.5, {className:'+=show active'}).to(".tab-pane-3", 0, {className:'-=show active'}).to(".tab-pane-4", 1.5, {className:'+=show active'});
 
-
-let tween1;
-tween1 = TweenMax.to(".third-slide__container-3", 0.5, {opacity : 1, left: '0px', className:'+=stage-third'});
-let scene1 = new ScrollMagic.Scene({triggerElement: "#trigger3", duration: "300px", offset: 0})
-    .setTween(tween1)
-    .addIndicators()
-    .addTo(controller);
-
-let tween1_1;
-tween1_1 = TweenMax.to(".third-slide__container-3", 0.5, {opacity : 0});
-let scene1_1 = new ScrollMagic.Scene({triggerElement: "#trigger3-1", duration: "200px", offset: 0})
-    .setTween(tween1_1)
-    .addIndicators()
-    .addTo(controller);
-
-
-//////////////
-
-
-let tween;
-tween = TweenMax.to(".third-slide__container-4", 0.5, {
-    opacity : 1,
-    left: '0px',
-    className:'+=stage-fourth'
-});
-let scene = new ScrollMagic.Scene({triggerElement: "#trigger4", duration: "300px", offset: 0})
-    .setTween(tween)
-    .addIndicators()
-    .addTo(controller);
+let scene3 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "1000", offset: -50}).setTween(tween3).setPin(".third-slide__container-1").addTo(controller).triggerHook(0);
+let scene2 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: "1000", offset: -50}).setTween(tween2).addTo(controller).triggerHook(0);
